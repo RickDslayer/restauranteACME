862e7f4a6339402ffa88b3c2e8be12b615365b1a
yonder maldonado <yondermaldonado23@gmail.com>
2026-04-07 15:59:11 -0500

    funciones de mesas

---
+++ mesas.py
@@ -0,0 +1,38 @@
+mesas = {}
+def crear_mesas(codigo, nombre,puestos):
+            if codigo in mesas:
+                print("ya existe este codigo de mesa")
+                print(f"{codigo}: {mesas[codigo]}")
+            else:
+                mesas[codigo] = (nombre,puestos)
+mesa_codigo = int(input("Dime el codigo:\n"))
+nombre = input("Dime el nombre de la mesa:\n")
+puestos = int(input("Dime el numero de puestos:\n"))
+crear_mesas(mesa_codigo,nombre,puestos)
+def lista_mesas():
+    for codigo, valores in mesas.items():
+        nom, pues = valores
+        print("--------------MESAS-------------")
+        print(f"{codigo}: {nom}, {pues}")
+def eliminar_mesa(codigo):
+        if codigo in mesas:
+            del(mesas[codigo])
+            print("Mesa eliminada")
+        else:
+            print("El codigo no existe")
+def editar_mesa(codigo):
+      if codigo in mesas:
+            nuevo_nombre = input("Dime el nuevo nombre:\n")
+            nuevo_puesto = int(input("Dime cuantos puestos:\n"))
+            mesas[codigo] = nuevo_nombre, nuevo_puesto
+      else:
+            print("No se encuentra la mesa")
+def facturacion(mesa,cliente):
+       if mesa in mesas:
+             print("")
+lista_mesas()
+a = input("D")
+editar_mesa(a)
+lista_mesas()
+#editar_mesa()
+#eliminar_mesa(mesa_codigo)
