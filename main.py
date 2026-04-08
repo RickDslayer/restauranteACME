b782fe2c851f1112c7f042ea8ccfcab6a489acd0
Jesus Salcedo <Tecnicajesus20@gmail.com>
2026-04-08 14:32:37 -0500

    nuevo

--- main.py
+++ main.py
@@ -8,7 +8,10 @@ while True:
         opcion = int(input(""))
         match opcion:
             case 1:
-                
                 producto_codigo = int(input("Ingrese el codigo del nuevo producto:\n"))
-                producto_nombre = input("")
-                productos.crear_producto()
+                producto_nombre = input("Ingrese el nombre:\n")
+                producto_valor_unitario = int(input("Ingrese el valor unitario:\n"))
+                producto_iva = int(input("Ingrese el valor del iva:\n"))
+                productos.crear_producto(producto_codigo,producto_nombre,producto_valor_unitario,producto_iva)
+    except ValueError:
+        print("ERROR de valor (solo numeros)")       
