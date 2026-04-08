f95d836400b46cde8124ee8c5268fc9c92427838
Jesus Salcedo <Tecnicajesus20@gmail.com>
2026-04-08 15:06:10 -0500

    menu hasta crear clientes

--- clientes.py
+++ clientes.py
@@ -1,4 +1,3 @@
-# Crear clientes
 Clientes = {
 }
 def crearClientes (C,name, tel, email):
@@ -8,25 +7,7 @@ def crearClientes (C,name, tel, email):
             print(f"Informacion del cliente:\nNombre = {n}\nCedula = {C}\nTelefono = {t}\nCorreo = {e}\n")      
     else:
         Clientes[C]= (name , tel , email)
-while True:
-    try :
-        Id = int (input("Digite la cedula del usuario:\n"))
-        Telefono = int (input("Digite el numero de telefono del usuario:\n"))
-        cantidadN = len(Telefono)
-        if cantidadN != 10 : 
-            print ("Un numero telefonico valido tiene 10 digitos.")
-        else:
-            break
-    except ValueError:
-        print("Digite solo numeros")
-    Nombre = input ("Digite su nombre de usuario:")
-    try:
-        Correo = input ("Digite el correo elctronico\nRecuerde que debe terminar en (gmail.com)")
-        if "@" not in Correo:
-            raise ValueError("error falta el @")
-        print(f"correo valido {Correo}")
-    except ValueError as ve:
-        print(ve)
+
         
         
         
--- main.py
+++ main.py
@@ -1,21 +1,39 @@
 import productos
 import mesas
+import clientes
 while True:
     try:
         print("--------------Menu---------------")
-        print("1. Crear productos\n2. Crear mesas\n3. Crear mesas\n4. Factura\n5. Reporte de ventas\n0. Salir")
+        print("1. Crear productos\n2. Crear mesas\n3. Crear clientes\n4. Factura\n5. Reporte de ventas\n0. Salir")
         opcion = int(input(""))
         match opcion:
             case 1:
-                    producto_codigo = int(input("Ingrese el codigo del nuevo producto (solo numeros):\n"))
-                    producto_nombre = input("Ingrese el nombre:\n")
-                    producto_valor_unitario = int(input("Ingrese el valor unitario (solo numeros):\n"))
-                    producto_iva = int(input("Ingrese el valor del iva (solo numeros):\n"))
-                    productos.crear_producto(producto_codigo,producto_nombre,producto_valor_unitario,producto_iva)
+                producto_codigo = int(input("Ingrese el codigo del nuevo producto (solo numeros):\n"))
+                producto_nombre = input("Ingrese el nombre:\n")
+                producto_valor_unitario = int(input("Ingrese el valor unitario (solo numeros):\n"))
+                producto_iva = int(input("Ingrese el valor del iva (soo numeros):\n"))
+                productos.crear_producto(producto_codigo,producto_nombre,producto_valor_unitario,producto_iva)
             case 2:
-                  mesa_codigo = int(input("Ingresa el codigo del la nueva mesa (solo numeros):\n"))
-                  mesa_nombre = input("Ingresa el nombre de la mesa:\n")
-                  mesa_puestos = int(input("Ingresa la cantidad de puestos (solo numeros):\n"))
-                  mesas.crear_mesas(mesa_codigo,mesa_nombre,mesa_puestos)
+                mesa_codigo = int(input("Ingresa el codigo del la nueva mesa (solo numeros):\n"))
+                mesa_nombre = input("Ingresa el nombre de la mesa:\n")
+                mesa_puestos = int(input("Ingresa la cantidad de puestos (solo numeros):\n"))
+                mesas.crear_mesas(mesa_codigo,mesa_nombre,mesa_puestos)
+            case 3:
+                while True:
+                    try :
+                        Id = int(input("Digite la cedula del usuario:\n"))
+                        Telefono = int(input("Digite el numero de telefono del usuario:\n"))
+                    except ValueError:
+                        print("Digite solo numeros")
+                    Nombre = input ("Digite su nombre de usuario:")
+                    try:
+                        Correo = input ("Digite el correo elctronico\nRecuerde que debe terminar en (gmail.com)")
+                        if "@" not in Correo:
+                            raise ValueError("error falta el @")
+                        print(f"correo valido {Correo}")
+                    except ValueError as ve:
+                        print(ve)
+                    clientes.crearClientes(Id,Nombre,Telefono,Correo)
+                    break
     except ValueError:
-        print("ERROR de valor (solo numeros)")       
+            print("ERROR de valor (solo numeros)")       
